d2302a4aa4a76f73d9750497dbe5c49cbed3d58a
Patrick Ellis <patrick.j.ellis@gmail.com>
2023-12-07 19:49:09 -0500

    update browser test

--- browser_test.ts
+++ browser_test.ts
@@ -1,11 +1,22 @@
+/// <reference lib="deno.ns" />
+
 import * as log from "https://deno.land/std@0.208.0/log/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/assert_equals.ts";
 import { time } from "https://deno.land/x/time.ts@v2.0.1/mod.ts";
 import puppeteer, {
   Browser,
+  ElementHandle,
   Page,
 } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 import * as path from "https://deno.land/std@0.208.0/path/mod.ts";
+import {
+  colorChords,
+  FeatureFlagKeys,
+  FeatureFlagKeysType,
+  FeatureFlags,
+} from "./settings.ts";
+import { HTMLInputElement } from "./node_modules/typescript/lib/lib.dom.d.ts";
+import { assertArrayIncludes } from "https://deno.land/std@0.208.0/assert/assert_array_includes.ts";
 
 const indexAbsolutePath = path.resolve("./index.html");
 const songFilePath = path.resolve("./songs/chelsea_bridge.leadsheet");
@@ -37,14 +48,21 @@ Deno.test("index.html renders via file:// protocol", async () => {
     assertEquals("Fm", await getKey(page));
     assertEquals("Eb7", (await getChords(page))[0]);
 
-    await teardown(browser);
-  } catch {
+    await enableFeature(page, colorChords);
+
+    const firstChordsClasses = (await getChordClasses(page))[0];
+    assertArrayIncludes(firstChordsClasses, ["dom"]);
+  } catch (e) {
+    log.info(e);
     const filename = path.join(
       screenshotsPath,
       `test-failure-${time().now().getSeconds()}.png`,
     );
-    log.info(`🚨 test failure screenshot saved to: ${filename}`);
+    log.info(`\n🚨 test failure screenshot saved to: ${filename}`);
+
     await page.screenshot({ path: filename });
+  } finally {
+    await teardown(browser);
   }
 });
 
@@ -81,10 +99,11 @@ async function getTransposedAmount(page: Page) {
 }
 
 async function getChords(page: Page) {
-  const chords: Array<string> = await (await page.$("#song"))!.$$eval(
-    ".chord",
-    (es) => es.map((e) => e.textContent).filter(String),
-  );
+  const chords: Array<string> = await (await page.waitForSelector("#song"))!
+    .$$eval(
+      ".chord",
+      (es) => es.map((e) => e.textContent).filter(String),
+    );
   return chords;
 }
 
@@ -99,4 +118,21 @@ async function transpose(page: Page, steps: number) {
 async function uploadFile(page: Page, filepath: string) {
   const input = await page.waitForSelector("#songfile");
   await input!.uploadFile(filepath);
+}
+
+async function enableFeature(page: Page, flag: FeatureFlagKeysType) {
+  const input = <ElementHandle<HTMLInputElement>> await page.waitForSelector(
+    `#settings input[name="${flag}"]`,
+  );
+
+  return await input?.click();
+}
+
+async function getChordClasses(page: Page) {
+  const chords: string[][] = await (await page.waitForSelector("#song"))!
+    .$$eval(
+      ".chord",
+      (es): string[][] => es.map((e) => Object.values(e.classList)),
+    );
+  return chords;
 }
