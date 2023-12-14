71354cf4ef3fc8900659f7143da6fb9d43b9f973
Patrick Ellis <patrick.j.ellis@gmail.com>
2023-12-14 18:05:41 -0500

    layout tweaks: use container queries for most font sizes

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// css imports are just here so esbuild processes the css file
-import "./style/style.css";
-import "./style/atoms.css";
 import { bootstrap } from "./app/app.ts";
 
 window.onload = bootstrap;
