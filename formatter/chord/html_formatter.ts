301f4643b6088cc5538b06c4a9487b88d3e0cbcc
Patrick Ellis <patrick.j.ellis@gmail.com>
2023-12-28 22:07:11 -0500

    first pass at html chord formatter

--- formatter/chord/html_formatter.ts
+++ formatter/chord/html_formatter.ts
@@ -3,11 +3,30 @@ import {
   type AlterableDegree,
   AlterAdd,
   type Alteration,
+  AlterCompound,
+  AlterEverything,
   AlterLower,
+  AlterMajor,
+  AlterMinor,
   AlterOmit,
   AlterRaise,
   AlterSuspend,
 } from "../../theory/chord.ts";
+import { Power_id } from "../../theory/chord/quality/dyad.ts";
+import {
+  Aug7_id,
+  Dim7_id,
+  DimM7_id,
+  Dom7_id,
+  Maj6_id,
+  Maj6Sh5_id,
+  Maj7_id,
+  Maj7Sh5_id,
+  Min6_id,
+  Min7_id,
+  Min7Fl5_id,
+  MinMaj7_id,
+} from "../../theory/chord/quality/tetrad.ts";
 import {
   Aug_id,
   Dim_id,
@@ -17,22 +36,45 @@ import {
 import { FlatSymbol, SharpSymbol } from "../../theory/notation.ts";
 import { TextFormatter } from "./text_formatter.ts";
 
+const M = "M" as const;
+const m = "m" as const;
+
 export const HTMLFormatter = class extends TextFormatter {
   override symbols = {
     ...(new TextFormatter()).symbols,
-    "#": unicodeify("#"),
-    "b": unicodeify("b"),
+    "#": uni("#"),
+    "b": uni("b"),
     quality: {
       ...(new TextFormatter()).symbols.quality,
       [Aug_id]: "⁺" as const,
       [Dim_id]: "°" as const,
       [Maj_id]: "" as const,
-      [Min_id]: "m" as const,
+      [Min_id]: m,
+      [Maj7_id]: (x: AlterableDegree) => `${M}${sup(x)}`,
+      [Dom7_id]: (x: AlterableDegree) => `${sup(x)}`,
+      [Min7_id]: (x: AlterableDegree) => `${m}${sup(x)}`,
+      [Aug7_id]: (x: AlterableDegree) => `⁺${sup(x)}`,
+      [Dim7_id]: (x: AlterableDegree) => `°${sup(x)}`,
+      [DimM7_id]: (x: AlterableDegree) => `°${sup(M)}${sup(x)}`,
+      [Maj7Sh5_id]: (x: AlterableDegree) => `⁺${sup(M)}${sup(x)}`,
+      [Min7Fl5_id]: (x: AlterableDegree) => `${m}${sup(x)}${uni("b")}${sup(5)}`,
+      [MinMaj7_id]: (x: AlterableDegree) => `${m}${sup(M)}${sup(x)}`,
+      [Maj6_id]: sup(6),
+      [Min6_id]: `${m}${sup(6)}`,
+      [Maj6Sh5_id]: `⁺${sup(6)}`,
+      [Power_id]: sup(5),
     },
     alteration: {
       ...(new TextFormatter()).symbols.alteration,
-      [AlterRaise]: (x: Alteration["target"]) => `${unicodeify("#")}${x}`,
-      [AlterLower]: (x: Alteration["target"]) => `${unicodeify("b")}${x}`,
+      [AlterRaise]: (x: Alteration["target"]) => `${uni("#")}${x}`,
+      [AlterLower]: (x: Alteration["target"]) => `${uni("b")}${x}`,
+      [AlterMajor]: (x: Alteration["target"]) => `${sup(M)}${sup(x)}`,
+      [AlterMinor]: (x: Alteration["target"]) => `${sup(m)}${sup(x)}`,
+      [AlterAdd]: (x: Alteration["target"]) => `${low("add")}${x}`,
+      [AlterOmit]: (x: Alteration["target"]) => `${low("no")}${x}`,
+      [AlterCompound]: (x: Alteration["target"]) => `/${x}`,
+      [AlterSuspend]: (x: Alteration["target"]) => `${low("sus")}${x}`,
+      [AlterEverything]: (x: Alteration["target"]) => `${sup(x)}${low("alt")}`,
     },
   };
 
@@ -70,8 +112,19 @@ export const HTMLFormatter = class extends TextFormatter {
   }
 };
 
-function unicodeify(str: "#" | "b"): string {
+// unicode-ify the sharp or flat (returns a custom <span> element)
+function uni(str: "#" | "b"): string {
   return `<span class="unicode-${str === "#" ? "sharp" : "flat"}">${
     str === "#" ? SharpSymbol : FlatSymbol
   }</span>`;
+}
+
+// wrap it in a <sup> (superscript) tag
+function sup(s: number | string): string {
+  return `<sup class="extent">${s}</sup>`;
+}
+
+// make the text slightly smaller
+function low(s: string): string {
+  return `<span class="deemphasize">${s}</span>`;
 }
